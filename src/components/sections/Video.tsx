3c620102c5464e3592d527708f6689b70310437b
sbjang <sbjang123456@gmail.com>
2024-06-28 10:41:56 +0900

    feat: 청첩장 메인 video 컴포넌트 구현

--- src/components/sections/Video.tsx
+++ src/components/sections/Video.tsx
@@ -5,5 +5,11 @@ import { Section } from '@/components/shared';
 const cx = classNames.bind(styles);
 
 export function Video() {
-  return <Section>Video</Section>;
+  return (
+    <Section className={cx('container')}>
+      <video autoPlay muted loop controls poster="/assets/poster.jpg">
+        <source src="/assets/main.mp4" type="video/mp4" />
+      </video>
+    </Section>
+  );
 }
