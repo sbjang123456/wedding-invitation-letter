5ecbc66b376f53dcc451e43be849fea13ee3ef7f
sbjang <sbjang123456@gmail.com>
2024-07-01 13:16:09 +0900

    feat: 이미지 갤러리 > 상세 및 캐러셀 구현

--- src/components/sections/ImageGallery.tsx
+++ src/components/sections/ImageGallery.tsx
@@ -1,6 +1,8 @@
 import classNames from 'classnames/bind';
 import styles from './ImageGallery.module.scss';
 import { Section } from '@/components/shared';
+import { ImageViewer } from '@/components/ImageViewer';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
@@ -9,15 +11,37 @@ type ImageGalleryProps = {
 };
 
 export function ImageGallery({ images }: ImageGalleryProps) {
+  const [selectedIdx, setSelectedIdx] = useState(-1);
+
+  const open = selectedIdx > -1;
+
+  const handleSelectedImage = (idx: number) => () => {
+    setSelectedIdx(idx);
+  };
+
+  const handleClose = () => {
+    setSelectedIdx(-1);
+  };
+
   return (
     <Section title="사진첩">
       <ul className={cx('wrap-images')}>
         {images.map((src, idx) => (
-          <li key={idx} className={cx('wrap-image')}>
+          <li
+            key={idx}
+            className={cx('wrap-image')}
+            onClick={handleSelectedImage(idx)}
+          >
             <img src={src} alt="사진첩 이미지" />
           </li>
         ))}
       </ul>
+      <ImageViewer
+        images={images}
+        open={open}
+        selectedIdx={selectedIdx}
+        onClose={handleClose}
+      />
     </Section>
   );
 }
